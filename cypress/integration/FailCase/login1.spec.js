f2b2f7d06394a20d3bf1a9352e0e81b77790e2a0
bhosalenityanand <bhosalenityanand5@gmail.com>
2022-08-06 17:52:24 +0530

    mocha

---
+++ cypress/integration/FailCase/login1.spec.js
@@ -0,0 +1,6 @@
+/// <reference types="cypress" />
+describe('verify the login',function(){
+    it('verify with valid credential',function(){
+        expect(1).to.eqls(2)
+    })
+})
---
+++ cypress/integration/PassCase/login2.spec.js
@@ -0,0 +1,7 @@
+
+/// <reference types="cypress" />
+describe('verify the login',function(){
+    it('verify with valid credential',function(){
+        expect(1).to.eqls(1)
+    })
+})
